1d841f44bc0311d009ad94d95a9c637ab0fce070
Ben Smith <bensmith@mediaworks.co.nz>
2021-06-02 12:59:09 +1200

    helper for validating all discord command parameters

--- src/discord-api/discord-helpers.js
+++ src/discord-api/discord-helpers.js
@@ -3,6 +3,9 @@ const nacl = require('tweetnacl');
 
 const { loadInvestmentConfig } = require('../database');
 
+const discordCommands = require('../data/discordCommands.json');
+const decimalValueMap = require('../data/decimalValueMap.json');
+
 
 function errorResponse(err, statusCode = 400) {
 	return {
@@ -13,7 +16,15 @@ function errorResponse(err, statusCode = 400) {
 
 
 function respondToPing() {
-	return JSON.stringify({ type: 1 });
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			type: 4,
+			data: {
+				content: JSON.stringify({ type: 1 }),
+			},
+		}),
+	};
 }
 
 
@@ -35,6 +46,99 @@ function requestIsValid({ headers, body }) {
 
 
 /**
+ * Returns the input parameter of the discord slash command if it exists
+ *
+ * @param {string} paramName - name of the slash command parameter
+ * @param {object} inputOptions
+ */
+function getInputValue(paramName, inputOptions) {
+	if (!inputOptions) return null;
+
+	const param = inputOptions.find(option => (option.name === paramName));
+
+	return param?.value || null;
+}
+
+
+/**
+ *
+ * @param {string} commandName
+ * @returns
+ */
+function getCommandDetails(commandName) {
+	return discordCommands.find(c => c.name === commandName);
+}
+
+
+/**
+ * Returns an array of errors based on the given parameter options and the
+ * command parameter details specified in the discordCommands.json
+ *
+ * @param {object} body - body of the incoming request
+ * @param {object} commandDetails
+ * @returns {array}
+ */
+function validateCommandParams(body, commandDetails) {
+
+	if (!commandDetails.options || !commandDetails.options.length) {
+		return []; // no options specified in the commands to validate
+	}
+
+	const inputOptions = body.data?.options;
+
+	if (!inputOptions) {
+		return ['Internal error - options were specified in commands list but don\'t exist on the request'];
+	}
+
+	const validationErrors = inputOptions.map(option => {
+
+		const optionValue = getInputValue(option.name, inputOptions);
+
+		// skip validation if it isn't required and wasn't provided
+		if (!option.required && !optionValue) {
+			return null;
+		}
+
+		switch (option.name) {
+
+		case 'code': {
+			const foundCurrency = decimalValueMap.find(crypto => crypto.base_currency === optionValue);
+
+			return foundCurrency
+				? null
+				: `Currency '${optionValue}' is invalid or does not exist in crypto.com exchange - use /list-available-crypto for the full list`;
+		}
+
+		case 'sell-percentage': {
+			return optionValue > 0
+				? null
+				: `Invalid input '${optionValue}' - ${option.name} must be a positive number`;
+		}
+
+		case 'buy-percentage' || 'stop-loss-percentage' || 'warning-percentage': {
+			return optionValue < 0
+				? null
+				: `Invalid input '${optionValue}' - ${option.name} must be a negative number`;
+		}
+
+		case 'limit-amount': {
+			return optionValue >= 1
+				? null
+				: `Invalid input '${optionValue}' - ${option.name} must be at least $1 USDT`;
+		}
+
+		default: {
+			return `Internal error - '${option.name}' was sent on the request but has no validation`;
+		}
+
+		}
+	});
+
+	return validationErrors;
+}
+
+
+/**
  * Returns the user database configuration
  *
  * @param {string} userId
@@ -52,5 +156,8 @@ module.exports = {
 	errorResponse,
 	respondToPing,
 	requestIsValid,
+	getInputValue,
+	getCommandDetails,
+	validateCommandParams,
 	getUserConfiguration,
 };
