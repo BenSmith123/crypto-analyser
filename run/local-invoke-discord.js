06cd1bc0db04c7c64c911670787b12fc8c19eb97
BenSmith123 <benjiiman12@gmail.com>
2021-06-02 15:35:55 +1200

    configurable body.json in discord command debugging

--- run/local-invoke-discord.js
+++ run/local-invoke-discord.js
@@ -5,12 +5,14 @@
 const lambda = require('../src/discord-api/index');
 
 const mockDiscordCommand = require('../resources/lambda/lambda-api-gateway-discord.json');
+const mockBody = require('../resources/lambda/discord-api-gateway-body.json');
 
 
 (async () => {
 
 	console.log('Running lambda function code..');
 
+	mockDiscordCommand.body = JSON.stringify(mockBody);
 	const response = await lambda.discordController(mockDiscordCommand);
 
 	console.log('Lambda function execution finished:', response);
