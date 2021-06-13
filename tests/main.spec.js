8a656e0bafc5eaae42eef880622fef566aaaa4af
Ben Smith <bensmith@mediaworks.co.nz>
2021-06-13 21:01:40 +1200

    implement test for sell scenario

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,44 +1,28 @@
 
 /* eslint-disable no-underscore-dangle */
+/* eslint-disable global-require */
 /* eslint-disable no-console */
-
 
 const sinon = require('sinon');
 const rewire = require('rewire');
 const { assert } = require('chai');
 
-
 const { getLogs } = require('../src/logging');
 const cryptoModule = require('../src/crypto');
 const helperModule = require('../src/helpers');
-// const databaseModule = require('../src/database');
+const { investmentConfigIsValid } = require('../src/database');
 
 
 let spyformatPriceLog;
 
-
 setupStubs();
 // ^ with object destructuring in index.js this has to be stubbed BEFORE index.js is required
 
-
 const index = rewire('../src/index');
-
-const { error } = require('./database-config/error.json');
-
-
-function overrideLogToDiscord(message, isAlert) {
-
-	if (typeof message !== 'string') {
-		console.log(JSON.stringify(message, null, 4).replace(/"|,/g, ''));
-		return;
-	}
-
-	console.log(message);
-}
 
 
 function setupStubs() {
-	// sinon.stub(cryptoModule, 'getAllCryptoValues').returns('ffs');
+
 	sinon.stub(helperModule, 'logToDiscord').callsFake(overrideLogToDiscord);
 
 	const mockCryptoValue = {
@@ -55,47 +39,82 @@ function setupStubs() {
 		CRO: { balance: 0.01879796, available: 0.01879796, order: 0, stake: 0, currency: 'CRO' },
 	};
 
-
+	// override return values of external functions
 	sinon.stub(cryptoModule, 'getAllCryptoValues').returns(mockCryptoValue);
 	sinon.stub(cryptoModule, 'getAccountSummary').returns(mockAccount);
 	sinon.stub(cryptoModule, 'checkLatestValueTrend').returns(false);
-	sinon.stub(cryptoModule, 'placeSellOrder');
+	sinon.stub(cryptoModule, 'placeSellOrder').returns({ result: { order_id: '078340' } });
 	sinon.stub(cryptoModule, 'processPlacedOrder').returns(12.4);
 
-
 	spyformatPriceLog = sinon.spy(helperModule, 'formatPriceLog');
-
 }
 
 
-describe('model/users', () => {
+const testScenarios = [
+	{
+		description: 'Should successfully place a sell order',
+		inputConfig: require('./database-config/sellExample.json'),
+	},
+];
 
+
+testScenarios.forEach(({ description, inputConfig }) => describe('#makeCryptoCurrenciesTrades', () => {
+
+	let makeCryptoCurrenciesTrades;
 
 	before(() => {
-
-
 		// sinon.stub(databaseModule, 'loadInvestmentConfig').returns('hello');
+		makeCryptoCurrenciesTrades = index.__get__('makeCryptoCurrenciesTrades');
 	});
 
-	const array = [1];
+	it(description, async () => {
 
-	array.forEach(element => {
+		const { config, ordersPlaced } = await makeCryptoCurrenciesTrades(inputConfig);
 
-		it('', async () => {
+		assert.isTrue(investmentConfigIsValid(config), 'should be updated and valid for next run');
 
+		const currentRecord = config.records.DOGE;
 
-			const makeCryptoCurrenciesTrades = index.__get__('makeCryptoCurrenciesTrades');
+		assert.equal(currentRecord.isHolding, false);
+		assert.equal(currentRecord.limitUSDT, 384);
+		assert.equal(currentRecord.lastSellPrice, 12.4);
 
+		assert.equal(currentRecord.thresholds.buyPercentage, -1);
+		assert.equal(currentRecord.thresholds.sellPercentage, 3);
+		assert.equal(currentRecord.thresholds.stopLossPercentage, -10);
 
-			const results = await makeCryptoCurrenciesTrades(error);
+		assert.equal(ordersPlaced.length, 1);
 
-			getLogs();
+		// TODO - maybe test this individually and check values less specifically
+		const expectedOrderObj = {
+			type: 'SELL',
+			name: 'DOGE',
+			amount: 31,
+			valuePlaced: 0.4,
+			valueFilled: 12.4,
+			difference: '+14.67%',
+			quantity: '384.40 USD',
+			summary: 'Sell order FILLED for 31 DOGE at $12.4 USD',
+			orderId: '078340',
+		};
 
-			assert.isString(console.log(spyformatPriceLog.returnValues[0]));
+		assert.isString(ordersPlaced[0].date);
+		delete ordersPlaced[0].date; // date is dynamic, remove before comparing
 
-		});
+		assert.deepEqual(ordersPlaced[0], expectedOrderObj);
 
+		console.log(getLogs());
 	});
 
+}));
 
-});
+
+function overrideLogToDiscord(message, isAlert) {
+
+	if (typeof message !== 'string') {
+		console.log(JSON.stringify(message, null, 4).replace(/"|,/g, ''));
+		return;
+	}
+
+	console.log(message);
+}
