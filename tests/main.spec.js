7c248ed5ca38ee01e19bc43a134092b1a30d33d1
Ben Smith <bensmith@mediaworks.co.nz>
2021-06-14 16:04:32 +1200

    working unit tests on placing selling orders

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -14,99 +14,17 @@ const { investmentConfigIsValid } = require('../src/database');
 
 
 let spyformatPriceLog;
-
-setupStubs();
-// ^ with object destructuring in index.js this has to be stubbed BEFORE index.js is required
-
-const index = rewire('../src/index');
+const stubs = {};
 
 
-function setupStubs() {
+const mockAccount = {
+	BTC: { balance: 0.005, available: 0.005, currency: 'BTC' },
+	DOGE: { balance: 31, available: 31, currency: 'DOGE' },
+	USDT: { balance: 8.8377054, available: 8.8377054, currency: 'USDT' },
+	CRO: { balance: 0.01879796, available: 0.01879796, currency: 'CRO' },
+};
 
-	sinon.stub(helperModule, 'logToDiscord').callsFake(overrideLogToDiscord);
-
-	const mockCryptoValue = {
-		DOGE: {
-			bestBid: 0.4,
-			bestAsk: 0.3,
-		},
-	};
-
-	const mockAccount = {
-		BTC: { balance: 0.00026717, available: 0.00026717, order: 0, stake: 0, currency: 'BTC' },
-		DOGE: { balance: 31, available: 31, order: 0, stake: 0, currency: 'DOGE' },
-		USDT: { balance: 8.8377054, available: 8.8377054, order: 0, stake: 0, currency: 'USDT' },
-		CRO: { balance: 0.01879796, available: 0.01879796, order: 0, stake: 0, currency: 'CRO' },
-	};
-
-	// override return values of external functions
-	sinon.stub(cryptoModule, 'getAllCryptoValues').returns(mockCryptoValue);
-	sinon.stub(cryptoModule, 'getAccountSummary').returns(mockAccount);
-	sinon.stub(cryptoModule, 'checkLatestValueTrend').returns(false);
-	sinon.stub(cryptoModule, 'placeSellOrder').returns({ result: { order_id: '078340' } });
-	sinon.stub(cryptoModule, 'processPlacedOrder').returns(12.4);
-
-	spyformatPriceLog = sinon.spy(helperModule, 'formatPriceLog');
-}
-
-
-const testScenarios = [
-	{
-		description: 'Should successfully place a sell order',
-		inputConfig: require('./database-config/sellExample.json'),
-	},
-];
-
-
-testScenarios.forEach(({ description, inputConfig }) => describe('#makeCryptoCurrenciesTrades', () => {
-
-	let makeCryptoCurrenciesTrades;
-
-	before(() => {
-		// sinon.stub(databaseModule, 'loadInvestmentConfig').returns('hello');
-		makeCryptoCurrenciesTrades = index.__get__('makeCryptoCurrenciesTrades');
-	});
-
-	it(description, async () => {
-
-		const { config, ordersPlaced } = await makeCryptoCurrenciesTrades(inputConfig);
-
-		assert.isTrue(investmentConfigIsValid(config), 'should be updated and valid for next run');
-
-		const currentRecord = config.records.DOGE;
-
-		assert.equal(currentRecord.isHolding, false);
-		assert.equal(currentRecord.limitUSDT, 384);
-		assert.equal(currentRecord.lastSellPrice, 12.4);
-
-		assert.equal(currentRecord.thresholds.buyPercentage, -1);
-		assert.equal(currentRecord.thresholds.sellPercentage, 3);
-		assert.equal(currentRecord.thresholds.stopLossPercentage, -10);
-
-		assert.equal(ordersPlaced.length, 1);
-
-		// TODO - maybe test this individually and check values less specifically
-		const expectedOrderObj = {
-			type: 'SELL',
-			name: 'DOGE',
-			amount: 31,
-			valuePlaced: 0.4,
-			valueFilled: 12.4,
-			difference: '+14.67%',
-			quantity: '384.40 USD',
-			summary: 'Sell order FILLED for 31 DOGE at $12.4 USD',
-			orderId: '078340',
-		};
-
-		assert.isString(ordersPlaced[0].date);
-		delete ordersPlaced[0].date; // date is dynamic, remove before comparing
-
-		assert.deepEqual(ordersPlaced[0], expectedOrderObj);
-
-		console.log(getLogs());
-	});
-
-}));
+// spyformatPriceLog = sinon.spy(helperModule, 'formatPriceLog');
 
 
 function overrideLogToDiscord(message, isAlert) {
@@ -118,3 +36,127 @@ function overrideLogToDiscord(message, isAlert) {
 
 	console.log(message);
 }
+
+
+describe('#makeCryptoCurrenciesTrades', () => {
+
+	let makeCryptoCurrenciesTrades;
+
+	before(() => {
+		sinon.stub(helperModule, 'logToDiscord').callsFake(overrideLogToDiscord);
+
+		sinon.stub(cryptoModule, 'getAccountSummary').returns(mockAccount);
+		sinon.stub(cryptoModule, 'checkLatestValueTrend').returns(false);
+		sinon.stub(cryptoModule, 'placeSellOrder').returns({ result: { order_id: '078340' } });
+	});
+
+	afterEach(() => {
+		stubs.getAllCryptoValues.restore();
+		stubs.processPlacedOrder.restore();
+	});
+
+	describe('Feature: Standard sell order', () => {
+
+		before(async () => {
+
+			const mockCryptoValue = {
+				DOGE: { bestBid: 0.4, bestAsk: 0.3 },
+			};
+
+			stubs.getAllCryptoValues = sinon.stub(cryptoModule, 'getAllCryptoValues').returns(mockCryptoValue);
+			stubs.processPlacedOrder = sinon.stub(cryptoModule, 'processPlacedOrder').returns(12.4);
+
+			const index = rewire('../src/index');
+			makeCryptoCurrenciesTrades = index.__get__('makeCryptoCurrenciesTrades');
+			// ^ with object destructuring in index.js this has to be stubbed BEFORE index.js is rewired
+
+		});
+
+		it('should have updated the current record of the config', async () => {
+
+			const inputConfig = require('./database-config-mock/sell.json');
+
+			const { config, ordersPlaced } = await makeCryptoCurrenciesTrades(inputConfig);
+
+			// test database configuration
+			assert.isTrue(investmentConfigIsValid(config), 'expect config to be valid for next run');
+
+			const currentRecord = config.records.DOGE;
+
+			assert.equal(currentRecord.isHolding, false);
+			assert.equal(currentRecord.limitUSDT, 384);
+			assert.equal(currentRecord.lastSellPrice, 12.4);
+
+			assert.equal(currentRecord.thresholds.buyPercentage, -1);
+			assert.equal(currentRecord.thresholds.sellPercentage, 3);
+			assert.equal(currentRecord.thresholds.stopLossPercentage, -10);
+
+			// maybe test this individually and check values less specifically
+			const expectedOrderObj = {
+				type: 'SELL',
+				name: 'DOGE',
+				amount: 31,
+				valuePlaced: 0.4,
+				valueFilled: 12.4,
+				difference: '+14.67%',
+				quantity: '384.40 USD',
+				summary: 'Sell order FILLED for 31 DOGE at $12.4 USD',
+				orderId: '078340',
+			};
+
+			assert.equal(ordersPlaced.length, 1);
+
+			assert.isString(ordersPlaced[0].date);
+			delete ordersPlaced[0].date; // date is dynamic, remove before comparing
+
+			assert.deepEqual(ordersPlaced[0], expectedOrderObj);
+
+			console.log(getLogs());
+		});
+
+	});
+
+
+	describe('Feature: Stop-loss sell order', () => {
+
+		before(async () => {
+
+			const mockCryptoValue = {
+				BTC: { bestBid: 0.248, bestAsk: 0.2475 },
+			};
+
+			stubs.getAllCryptoValues = sinon.stub(cryptoModule, 'getAllCryptoValues').returns(mockCryptoValue);
+			stubs.processPlacedOrder = sinon.stub(cryptoModule, 'processPlacedOrder').returns(0.25);
+
+			const index = rewire('../src/index');
+			makeCryptoCurrenciesTrades = index.__get__('makeCryptoCurrenciesTrades');
+		});
+
+		it('should have valid config for next run', async () => {
+
+			const inputConfig = require('./database-config-mock/sellStoploss.json');
+
+			const { config, ordersPlaced } = await makeCryptoCurrenciesTrades(inputConfig);
+
+			// test database configuration
+			assert.isTrue(investmentConfigIsValid(config), 'expect config to be valid for next run');
+
+			const currentRecord = config.records.BTC;
+
+			// const testThis = {
+			// 	limitUSDT: 10,
+			// 	isHolding: false,
+			// 	orderDate: '14/06/2021, 15:45pm',
+			// 	timestamp: 1623642347339,
+			// 	thresholds: { stopLossPercentage: -1, sellPercentage: 32.99, buyPercentage: 1 },
+			// 	breakEvenPrice: 0.3487833,
+			// 	pauseAfterSell: true,
+			// 	isAtLoss: true,
+			// 	lastSellPrice: 0.25,
+			// };
+
+		});
+
+	});
+
+});
