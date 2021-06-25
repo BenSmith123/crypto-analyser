f4a0da2530545a1b65adb0b9ecda71b74a0b04ee
BenSmith123 <benjiiman12@gmail.com>
2021-06-25 15:30:07 +1200

    examples and vscode config for discord http

---
+++ run/local-invoke-discord-https.js
@@ -0,0 +1,19 @@
+/**
+ * Simulate an AWS Lambda invocation by passing the function an event
+ */
+
+const lambda = require('../src/discord-api/index');
+
+const mockDiscordCommand = require('../resources/lambda/lambda-api-gateway-http.json');
+
+
+(async () => {
+
+	console.log('Running lambda function code..');
+
+	const response = await lambda.discordController(mockDiscordCommand);
+
+	console.log('Lambda function execution finished:', response);
+
+})();
+
