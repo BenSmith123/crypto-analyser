5968dc99f9479ad7fa5225260f60ee08979b026e
Ben Smith <bensmith@mediaworks.co.nz>
2021-08-12 21:05:06 +1200

    script to get missing transaction data

---
+++ scripts/saveMissingTransactions.js
@@ -0,0 +1,57 @@
+/**
+ * The Crypto.com exchange API has an unresolved bug where at least half of the transactions
+ * are not returned (even in a status 200 response).
+ *
+ * This script will retrieve the transactions listed for a given account and
+ * store them in the database.
+ *
+ * The transaction order ID's can be found in the user logs - they are logged here
+ * when there is a transaction retrieval issue.
+ */
+
+/* eslint-disable no-console */
+/* eslint-disable no-await-in-loop */
+
+const { getOrderDetail } = require('../src/crypto');
+const { saveTransaction } = require('../src/database');
+
+
+/// UPDATE ME ///
+process.env.API_KEY = '';
+process.env.API_SECRET = '';
+
+const ordersToSave = [
+	'1690510435207149122',
+];
+/// ///////// ///
+
+
+(async () => {
+
+	if (!process.env.API_KEY || !process.env.API_SECRET) {
+		throw new Error('Missing API key or secret');
+	}
+
+	try {
+
+		// don't bother stacking the promises just to make debugging easier
+		for (let i = 0; i < ordersToSave.length; i++) {
+			const orderId = ordersToSave[i];
+			const order = await getOrderDetail(orderId);
+
+			if (!order || !order.result || !Object.keys(order.result).length) {
+				console.log(`Error: Order ${orderId} not found`);
+			}
+
+			await saveTransaction(order);
+			console.log(`Order ${orderId} saved`);
+		}
+
+		console.log('Done! :)');
+
+	} catch (err) {
+		console.log(err);
+	}
+
+})();
+
