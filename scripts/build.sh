3d34e691d0eddea69d3031424b94eb2a56990c47
Ben Smith <bensmith@mediaworks.co.nz>
2021-08-27 20:31:50 +1200

    fix typescript config & build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,20 @@
 
 # typescript compile code to /dist folder
+echo -n "Compiling typescript files";
 tsc
 
+echo "Installing packages";
+cp package.json "./dist/";
 cd dist;
 npm install --production; # prod install - don't include dev deps
 
-zip -r lambda.zip ./*
+### Copying node_modules takes longer than just doing a fresh install..
+# echo "Copying node_modules into /dist folder"
+# mkdir -p dist/node_modules
+# cp -r ./node_modules/* ./dist/node_modules; # copy all code in src
 
-echo "Distribution .zip created in ./dist/lambda.zip!";
+echo "Zipping files"
+
+zip -r lambda.zip ./*;
+
+echo "Distribution .zip created: ./dist/lambda.zip!";
