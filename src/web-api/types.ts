cb6edd0851e6cc9912da6cadcb1e0bd291b43e6e
BenSmith123 <benjiiman12@gmail.com>
2021-10-26 22:11:51 +1300

    types/Transaction

--- src/web-api/types.ts
+++ src/web-api/types.ts
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 
 export interface UserConfiguration {
 	id: string,
@@ -28,3 +29,43 @@ export interface UserConfiguration {
 		firstName: string,
 	}
 }
+
+export interface Transaction {
+	status: string,
+    timestamp: number,
+    user: string,
+	order_info: {
+		side: 'SELL' | 'BUY',
+		cumulative_quantity: number,
+        quantity: number,
+		create_time: number,
+        fee_currency: string,
+		avg_price: number,
+        exec_inst: 'POST_ONLY' | '',
+        client_oid: string,
+        type: 'LIMIT' | 'MARKET' | 'STOP_LOSS' | 'STOP_LIMIT',
+        update_time: number,
+        time_in_force: 'GOOD_TILL_CANCEL' | 'IMMEDIATE_OR_CANCEL' | 'FILL_OR_KILL',
+        instrument_name: string,
+        price: number,
+        cumulative_value: number,
+        order_id: string,
+        status: 'ACTIVE' | 'CANCELED' | 'FILLED' | 'REJECTED' | 'EXPIRED'
+	},
+	trade_list: [
+		{
+			liquidity_indicator: 'MAKER' | 'TAKER',
+            side: 'SELL' | 'BUY',
+            trade_id: string,
+            create_time: number,
+            instrument_name: 'SUSHI_USDT',
+            fee: number,
+            fee_currency: string,
+			traded_quantity: number,
+            client_oid: string,
+            traded_price: number,
+            order_id: string
+		}
+	]
+}
+
